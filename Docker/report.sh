bcb0c89299f1705e76edd8763832715a92650040
Maarten de Keizer <bitbucket@maartendekeizer.nl>
2021-04-24 08:43:58 +0200

    Change paths in financial report script

--- Docker/report.sh
+++ Docker/report.sh
@@ -3,12 +3,12 @@
 startYear=$(date +'%Y')
 startDate="$startYear-01-01"
 endDate=$(date +'%Y-%m-%d')
-targetDir="/app/web/download"
+targetDir="/app/public/download"
 csvFile="$targetDir/factuur-report-$startDate-$endDate.csv"
 
 mkdir -p "$targetDir"
 
-php /app/app/console makkelijkemarkt:report:factuur $startDate $endDate --env=prod > "$csvFile"
+php /app/bin/console makkelijkemarkt:report:factuur $startDate $endDate --env=prod > "$csvFile"
 md5sum "$csvFile" > "$csvFile.md5"
 
 find "$targetDir" -type f -mtime +14 -delete
