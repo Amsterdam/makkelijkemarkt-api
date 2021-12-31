e948d97ee6daf74e9f06b5700e8eda2d91831ac6
johantenbroeke <johan@fullscrteen.nl>
2021-12-31 12:03:06 +0100

    changed test to extend ApiTestCase

--- tests/Controller/VervangerTestcase.php
+++ tests/Controller/VervangerTestcase.php
@@ -7,34 +7,14 @@ namespace App\Tests\Controller;
 use App\Entity\Token;
 use App\Entity\Koopman;
 use App\Entity\Vervanger;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Doctrine\Common\DataFixtures\Purger\ORMPurger;
-use Faker\Factory;
-use Faker\Generator;
-use GuzzleHttp\Client;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\PropertyAccess\PropertyAccess;
-use App\Controller\KoopmanController;
+use App\Test\ApiTestCase;
 
-class VervangerTestCase extends WebTestCase
+class VervangerTestCase extends ApiTestCase
 {
-    /** @var Client */
-    protected $client;
-
-    /** @var EntityManagerInterface $entityManager */
-    protected $em;
-
-    protected function setUp(): void{
-        $this->client = static::createClient(
-            array(), array('HTTP_HOST' => 'http://172.101.0.1:8081') 
-            );
-        $this->em = $this->client->getContainer()->get("doctrine.orm.default_entity_manager");
-    }
 
     public function testVervangerIdinKoopmanObject():void{
-        
+
+        $this->em = $this->entityManager; 
         $tokenRepository = $this->em->getRepository(Token::class);
         $vervangerRepository = $this->em->getRepository(Vervanger::class);
 
@@ -44,17 +24,9 @@ class VervangerTestCase extends WebTestCase
         $expected_vervanger_id = $vervanger->getVervangerId();
         $koopman_id = $vervanger->getKoopman()->getId();
 
-        $headers = [
-            'Content-Type' => 'application/json',
-            'HTTP_Authorization' => 'Bearer ' . $token->getUuid(),
-            'HTTP_MmAppKey' => 'insecure',
-        ];
-        var_dump($this->client);
-
-        $this->client->request('GET', '/api/1.1.0/koopman/id/'.$koopman_id, array(), array(), $headers);
-        $this->assertResponseIsSuccessful();
-        $resp = $this->client->getResponse();
-        $response_data = json_decode($resp->getContent(), true);
+        $response = $this->client->get('/api/1.1.0/koopman/id/'.$koopman_id, ['headers' => $this->headers]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $response_data = json_decode((string)$response->getBody(), true);
         $vervanger_id = $response_data['vervangers'][0]['vervanger_id'];
 
 	    $this->assertTrue($expected_vervanger_id === $vervanger_id);
