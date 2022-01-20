52d73c5e7c1765b82399c84c46a6cc7fa3a8cf03
Joey Hendriks <joey.hendriks@amsterdam.nl>
2022-01-20 16:12:59 +0100

    user truncate instead of delete from

--- testrunner/load_db.sh
+++ testrunner/load_db.sh
@@ -1,2 +1,2 @@
 APP_ENV=test /root/.symfony/bin/symfony console doctrine:migrations:migrate -n
-APP_ENV=test /root/.symfony/bin/symfony console doctrine:fixtures:load -n
+APP_ENV=test /root/.symfony/bin/symfony console doctrine:fixtures:load -n --purge-with-truncate
