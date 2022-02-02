3d702ce9f6a29900ac33154bad652df3cae3376d
Tim Fierens <t.fierens@amsterdam.nl>
2022-02-02 14:54:53 +0100

    add rsvp tests and fixtures

---
+++ tests/Controller/RsvpControllerTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Koopman;
+use App\Entity\Markt;
+use App\Test\ApiTestCase;
+use DateTime;
+
+class RsvpControllerTest extends ApiTestCase
+{
+    protected $marktDate;
+    protected $attending;
+    protected $marktId;
+    protected $koopmanErkenningsNummer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $marktRepository = $this->entityManager->getRepository(Markt::class);
+        $markt = $marktRepository->findOneBy([
+        ]);
+
+        $koopmanRepository = $this->entityManager->getRepository(Koopman::class);
+        $koopman = $koopmanRepository->findOneBy([
+        ]);
+
+        $this->marktDate = (new DateTime('now'))->format('Y-m-d');
+        $this->attending = true;
+        $this->marktId = $markt->getId();
+        $this->koopmanErkenningsNummer = $koopman->getErkenningsnummer();
+    }
+
+    protected function testRsvpGet($url)
+    {
+        $response = $this->client->get($url, ['headers' => $this->headers]);
+        $responseData = json_decode((string) $response->getBody(), true);
+        foreach ($responseData as $rsvp) {
+            $this->assertArrayHasKey('marktDate', $rsvp);
+            $this->assertArrayHasKey('attending', $rsvp);
+            $this->assertArrayHasKey('markt', $rsvp);
+            $this->assertArrayHasKey('koopman', $rsvp);
+        }
+        $this->assertIsArray($responseData);
+        $this->assertNotEmpty($responseData);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testPostRsvp()
+    {
+        $data = [
+            'marktDate' => $this->marktDate,
+            'attending' => $this->attending,
+            'marktId' => $this->marktId,
+            'koopmanErkenningsNummer' => $this->koopmanErkenningsNummer,
+        ];
+
+        $response = $this->client->post('/api/1.1.0/rsvp', ['headers' => $this->headers, 'body' => json_encode($data)]);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testGetRsvpByErkenningsnummer()
+    {
+        $this->testRsvpGet('/api/1.1.0/rsvp/koopman/'.$this->koopmanErkenningsNummer);
+    }
+
+    public function testGetRsvpByMarktIdAndMarktDate()
+    {
+        $this->testRsvpGet('/api/1.1.0/rsvp/markt/'.$this->marktId.'/date/'.$this->marktDate);
+    }
+
+    public function testGetRsvpByMarktIdAndErkenningsNummer()
+    {
+        $this->testRsvpGet('/api/1.1.0/rsvp/markt/'.$this->marktId.'/koopman/'.$this->koopmanErkenningsNummer);
+    }
+}
