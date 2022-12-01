5b02703e0b8e06f7759a48d6ceed6015427832a0
Ramon Hofmeister <r.hofmeister@amsterdam.nl>
2022-12-01 11:09:28 +0000

    seperate unpacking and adding photos and bestanden from each other

--- docker-phpfpm/import-mercato.sh
+++ docker-phpfpm/import-mercato.sh
@@ -14,10 +14,6 @@ cd $SRC_DIR
 echo "unpacking Bestanden.zip"
 stat Bestanden.zip
 unzip -qq -o Bestanden.zip -d $DST_DIR/
-echo "Done, unpacking Pasfotos.zip"
-stat Pasfotos.zip
-unzip -qq -o Pasfotos.zip -d $DST_DIR/fotos/
-echo "Done"
 
 # Import the CSV data into the database
 cd $DST_DIR
@@ -29,6 +25,14 @@ echo "Done, running import:perfectview:koopman"
 php /var/www/bin/console makkelijkemarkt:import:perfectview:koopman Koopman.CSV --env=prod
 echo "Done, running import:perfectview:sollicitatie"
 php /var/www/bin/console makkelijkemarkt:import:perfectview:sollicitatie Koopman_Markt.CSV --env=prod
+echo "Done"
+
+cd $SRC_DIR
+echo "Done, unpacking Pasfotos.zip"
+stat Pasfotos.zip
+unzip -qq -o Pasfotos.zip -d $DST_DIR/fotos/
+
+cd $DST_DIR
 echo "Done, running import:perfectview:foto"
 php /var/www/bin/console makkelijkemarkt:import:perfectview:foto Koopman.CSV fotos --env=prod
 echo "Done"
