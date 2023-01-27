f6ce05f5fed3d5579b6fe4ea130c20fca5625877
Ramon Hofmeister <ramon@Ramons-MacBook-Pro.local>
2023-01-27 14:41:10 +0100

    add grumphp fixes

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace App\EventListener;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class ExceptionListener
 {
