b66f1a604633c80cc92f108677a512c3c33b59e6
Ramon Hofmeister <ramon@Ramons-MacBook-Pro.local>
2023-12-01 14:35:49 +0100

    make everything extensible

--- src/Azure/AzureStorage.php
+++ src/Azure/AzureStorage.php
@@ -11,205 +11,106 @@ class AzureStorage
 {
     private HttpClientInterface $client;
 
-    private string $azureSubscriptionId;
-
-    private string $azureClientId;
-
-    private string $imageStorageId;
-
-    private string $azureAuthorityHost;
-
-    private string $azureTenantId;
-
-    private string $azureFederatedTokenFile;
-
     private SASImageReaderConfig $SASImageReaderConfig;
+
+    private array $config;
 
     private LoggerInterface $logger;
 
     public function __construct(
         HttpClientInterface $client,
         SASImageReaderConfig $SASImageReaderConfig,
-        string $azureSubscriptionId,
-        string $azureClientId,
-        string $imageStorageId,
-        LoggerInterface $logger,
-        string $azureAuthorityHost,
-        string $azureTenantId,
-        string $azureFederatedTokenFile
+        LoggerInterface $logger
     ) {
         $this->client = $client;
-        $this->azureSubscriptionId = $azureSubscriptionId;
-        $this->azureClientId = $azureClientId;
         $this->SASImageReaderConfig = $SASImageReaderConfig;
-        $this->imageStorageId = $imageStorageId;
         $this->logger = $logger;
-        $this->azureAuthorityHost = $azureAuthorityHost;
-        $this->azureTenantId = $azureTenantId;
-        $this->azureFederatedTokenFile = $azureFederatedTokenFile;
-        // $this->azureAuthorityHost = $azureAuthorityHost;
-        // $this->azureTenantId = $azureTenantId;
-        // $this->azureFederatedTokenFile = $azureFederatedTokenFile;
-        // $this->azureClientId = $azureClientId;
     }
 
-    // Generate a service SAS token.
-    // https://learn.microsoft.com/en-us/entra/identity/managed-identities-azure-resources/tutorial-linux-vm-access-storage-sas
-    // https://learn.microsoft.com/en-us/rest/api/storageservices/service-sas-examples
+    // Returns a url that is is signed with a SAS
     public function generateURLForImageReading($blob)
     {
-        $config = $this->SASImageReaderConfig->getConfig();
+        // Use a config to keep everything extensible
+        $this->config = $this->SASImageReaderConfig->getConfig();
 
-        $container = $config['container'];
-        $accountName = $config['accountName'];
-        $resourceType = $config['resourceType'];
-        $permissions = $config['permissions'];
-        // $key = $config['key'];
-        $sv = $config['sv'];
+        $accessToken = $this->getAccessToken();
 
-        $expired = (new \DateTime('now + 1 hour'))->format('Y-m-d\TH:i:s\Z');
-        $start = (new \DateTime('15 minutes ago'))->format('Y-m-d\TH:i:s\Z');
+        $signature = $this->getSAS($accessToken);
 
-        // First we need an access token to get a SAS from the resource manager
-        $accessToken = $this->getPasswordFromAzure();
-
-        $signature = $this->getSASFromResourceManager(
-            $accessToken,
-            $start,
-            $expired
-        );
-
-        $this->logger->warning('Signature: '.$signature);
-
-        // $signature = $this->getSASForBlob(
-        //     $accountName,
-        //     $container,
-        //     $blob,
-        //     $resourceType,
-        //     $permissions,
-        //     $expired,
-        //     $key
-        // );
-
-        /* Create the signed query part */
-        // $parts = array();
-        // $parts[] = (!empty($expired))?'se=' . urlencode($expired):'';
-        // $parts[] = 'sr=' . $resourceType;
-        // $parts[] = (!empty($permissions))?'sp=' . $permissions:'';
-        // $parts[] = 'sig=' . urlencode($signature);
-        // $parts[] = 'sv=' . urlencode($sv);
-
-        /* Create the signed blob URL */
+        // Create the signed blob URL
         $url = 'https://'
-            .$accountName.'.blob.core.windows.net/'
-            .$container.'/'
+            .$this->config['storageAccount'].'.blob.core.windows.net/'
+            .$this->config['imageContainer'].'/'
             .$blob.'?'
             .$signature;
-
-        $this->logger->warning('URL: '.$url);
 
         return $url;
     }
 
-    private function getSASFromResourceManager($accessToken, $start, $expired)
+    // Gets a SAS token from the resource manager using an access token
+    private function getSAS($accessToken)
     {
-        $response = $this->client->request('POST', 'https://management.azure.com/subscriptions/'.$this->azureSubscriptionId.'/resourceGroups/markten-o-rg/providers/Microsoft.Storage/storageAccounts/marktendataol5ct7bz3yely/listServiceSas?api-version=2019-06-01', [
-            RequestOptions::HEADERS => [
-                'Authorization' => 'Bearer '.$accessToken,
-                'Content-Type' => 'application/json',
-            ],
-            RequestOptions::JSON => [
-                'canonicalizedResource' => '/blob/marktendataol5ct7bz3yely/data',
-                'signedResource' => 'c', // TODO signed to the container, but could also be signed to specific blob.
-                'signedPermission' => 'r',
-                'signedProtocol' => 'https',
-                'signedExpiry' => $expired,
-                'signedStart' => $start,
-            ],
-        ]);
+        $url = 'https://management.azure.com/subscriptions/'
+            .$this->config['subscriptionId'].'/resourceGroups/'.$this->config['resourceGroup']
+            .'/providers/Microsoft.Storage/storageAccounts/'.$this->config['storageAccount']
+            .'/listServiceSas?api-version='.$this->config['apiVersion'];
+
+        $response = $this->client->request(
+            'POST',
+            $url,
+            [
+                RequestOptions::HEADERS => [
+                    'Authorization' => 'Bearer '.$accessToken,
+                    'Content-Type' => 'application/json',
+                ],
+                RequestOptions::JSON => [
+                    'canonicalizedResource' => '/blob/'.$this->config['storageAccount'].'/'.$this->config['container'],
+                    'signedResource' => $this->config['signedResource'],
+                    'signedPermission' => $this->config['permissions'],
+                    'signedProtocol' => 'https',
+                    'signedExpiry' => $this->config['expired'],
+                    'signedStart' => $this->config['start'],
+                ],
+            ]
+        );
 
         $body = $response->getContent(false);
         $data = json_decode($body, true);
-
-        $this->logger->warning('SAS: ', $data);
 
         return $data['serviceSasToken'];
     }
 
-    // private function getSASForBlob($accountName, $container, $blob, $resourceType, $permissions, $expiry,$key)
-    // {
-
-    //     /* Create the signature */
-    //     $arraySig = array();
-    //     $arraySig[] = $permissions;
-    //     $arraySig[] = '';
-    //     $arraySig[] = $expiry;
-    //     $arraySig[] = '/' . $accountName . '/' . $container . '/' . $blob;
-    //     $arraySig[] = '';
-    //     $arraySig[] = "2014-02-14"; //the API version is now required // TODO is there a newer version?
-    //     $arraySig[] = '';
-    //     $arraySig[] = '';
-    //     $arraySig[] = '';
-    //     $arraySig[] = '';
-    //     $arraySig[] = '';
-
-    //     // TODO DO WE NEED TO USE UTF8 ENCODE?
-    //     $string2sign = utf8_encode(implode("\n", $arraySig));
-
-    //     return base64_encode(
-    //         hash_hmac('sha256', urldecode($string2sign), base64_decode($key), true)
-    //     );
-    // }
-
-    // Get access token so we can obtain a SAS from the resource manager
-    private function getAccessTokenThroughManagedIdentity()
-    {
-        $response = $this->client->request('GET', 'http://169.254.169.254/metadata/identity/oauth2/token', [
-            RequestOptions::HEADERS => [
-                'Metadata' => 'true',
-            ],
-            RequestOptions::QUERY => [
-                'api-version' => '2018-02-01',
-                'resource' => 'https://management.azure.com/',
-                'client_id' => $this->azureClientId,
-                // 'resource_id' => $this->imageStorageId,
-            ],
-        ]);
-
-        if ($response->getStatusCode() >= 400) {
-            throw new \Exception(json_encode(['url' => $response->getInfo('url'), 'response' => $response->getContent(false), 'fullresponse' => $response->toArray(false)]));
-        }
-
-        $body = $response->getContent(false);
-        $data = json_decode($body, true);
-
-        // TODO should we cache this? probably.
-        $accessToken = $data['access_token'];
-
-        return $accessToken;
-    }
-
-    // TODO deze manier weer proberen
-    // https://learn.microsoft.com/en-us/entra/identity/managed-identities-azure-resources/tutorial-linux-vm-access-storage-sas
-    private function getPasswordFromAzure(): string
+    // Authenticate with federated token to get access token,
+    // which is used to get a SAS from the resource manager
+    private function getAccessToken(): string
     {
         // TODO implement caching
-        // TODO make this a bit more clean
-        $authorityHost = $this->azureAuthorityHost;
-        $tenantId = $this->azureTenantId;
-        $tokenUrl = "$authorityHost$tenantId/oauth2/v2.0/token";
+        $tokenUrl = $this->config['azureAuthorityHost'].$this->config['tenantId'].'/oauth2/v2.0/token';
         $grantType = 'client_credentials';
         $scope = 'https://management.azure.com//.default'; // double slash is on purpose
-        $clientAssertion = file_get_contents($this->azureFederatedTokenFile);
+        $clientAssertion = file_get_contents($this->config['federatedTokenFile']);
         $clientAssertionType = 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer';
-        $clientId = $this->azureClientId;
-        // Prepare the request payload
-        $payload = ['grant_type' => $grantType, 'scope' => $scope, 'client_assertion' => $clientAssertion, 'client_assertion_type' => $clientAssertionType, 'client_id' => $clientId];
 
-        $response = $this->client->request('POST', $tokenUrl, [RequestOptions::HEADERS => ['Content-Type' => 'application/x-www-form-urlencoded'], RequestOptions::BODY => http_build_query($payload)]);
+        // Prepare the request payload
+        $payload = [
+            'grant_type' => $grantType,
+             'scope' => $scope,
+             'client_assertion' => $clientAssertion,
+             'client_assertion_type' => $clientAssertionType,
+             'client_id' => $this->config['clientId'],
+        ];
+
+        $response = $this->client->request(
+            'POST',
+            $tokenUrl,
+            [
+                RequestOptions::HEADERS => ['Content-Type' => 'application/x-www-form-urlencoded'],
+                RequestOptions::BODY => http_build_query($payload),
+            ]
+        );
 
         if ($response->getStatusCode() >= 400) {
-            throw new \Exception(json_encode(['url' => $tokenUrl, 'payload' => $payload, 'response' => $response->getContent(false), 'fullresponse' => $response->toArray(false)]));
+            throw new \Exception(json_encode(['url' => $tokenUrl, 'response' => $response->getContent(false)]));
         }
 
         $body = $response->getContent(false);
--- src/Azure/Config/SASImageReaderConfig.php
+++ src/Azure/Config/SASImageReaderConfig.php
@@ -7,18 +7,24 @@ class SASImageReaderConfig
     private array $config;
 
     public function __construct(
-        $storageAccountName,
+        AzureBaseConfig $baseConfig,
+        $imageStorageAccount,
         $imageContainer
-        // $key
     ) {
-        $this->config = [
-            'accountName' => $storageAccountName,
-            'container' => $imageContainer,
-            'resourceType' => 'b',
-            'permissions' => 'r',
-            // 'key' => $key,
-            'sv' => '2023-01-01',
-        ];
+        $this->config = array_merge(
+            $baseConfig->getConfig(),
+            [
+                'storageAccount' => $imageStorageAccount,
+                'imageContainer' => $imageContainer,
+                'permissions' => 'r',
+                // specificies which resources are available
+                // currently signed to the container, but could also be signed to specific blob (b).
+                'signedResource' => 'c',
+                'apiVersion' => '2023-01-01',
+                'expiry' => (new \DateTime('now + 15 minutes'))->format('Y-m-d\TH:i:s\Z'),
+                'start' => (new \DateTime('15 minutes ago'))->format('Y-m-d\TH:i:s\Z'),
+            ]
+        );
     }
 
     public function getConfig(): array
