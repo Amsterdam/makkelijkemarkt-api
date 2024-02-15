ad7c52be855c7ef1d46ec9b477ea00c692823b8c
Ramon Hofmeister <ramon@Ramons-MacBook-Pro.local>
2024-02-15 15:57:40 +0100

    adds new db connection code from template

--- src/Doctrine/DynamicConnection.php
+++ src/Doctrine/DynamicConnection.php
@@ -7,31 +7,43 @@ use Doctrine\Common\EventManager;
 use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Driver;
+use Psr\Log\LoggerInterface;
 
 class DynamicConnection extends Connection
 {
-    private array $params;
-    private Driver $driver;
-    private ?Configuration $config;
-    private ?EventManager $eventManager;
-    private ?AzureDatabase $azureDatabase;
-
     public function __construct(
         array $params,
         Driver $driver,
         Configuration $config = null,
         EventManager $eventManager = null,
-        AzureDatabase $azureDatabase = null
+        private readonly ?AzureDatabase $azureDatabase = null,
+        private readonly ?LoggerInterface $logger = null,
     ) {
-        $this->params = $params;
-        $this->driver = $driver;
-        $this->config = $config;
-        $this->eventManager = $eventManager;
-        $this->azureDatabase = $azureDatabase;
-
-        if ($this->azureDatabase) {
-            $this->params['password'] = $this->azureDatabase->getPassword($this->params['password']);
+        if ($azureDatabase && $logger && isset($params['password'])) {
+            $newPassword = $azureDatabase->getPassword($params['password']);
+            $params = $this->addNewPasswordToParams($params, $newPassword);
         }
-        parent::__construct($this->params, $this->driver, $this->config, $this->eventManager);
+
+        parent::__construct($params, $driver, $config, $eventManager);
+
+        if ($azureDatabase && $logger && isset($params['password'])) {
+            try {
+                $this->connect();
+            } catch (\Exception $e) {
+                $this->logger->debug('DB Connection failed. Trying to invalidate cache and set password again.');
+                $newPassword = $azureDatabase->getPassword($params['password'], true);
+                $params = $this->addNewPasswordToParams($params, $newPassword);
+                parent::__construct($params, $driver, $config, $eventManager);
+                $this->connect();
+            }
+        }
+    }
+
+    private function addNewPasswordToParams(array $params, string $newPassword): array
+    {
+        $params['password'] = $newPassword;
+        $params['url'] = str_replace('temporary', $newPassword, $params['url']);
+
+        return $params;
     }
 }
--- src/Doctrine/DynamicConnectionFactory.php
+++ src/Doctrine/DynamicConnectionFactory.php
@@ -6,22 +6,22 @@ use App\Azure\AzureDatabase;
 use Doctrine\Bundle\DoctrineBundle\ConnectionFactory as BaseConnectionFactory;
 use Doctrine\Common\EventManager;
 use Doctrine\DBAL\Configuration;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class DynamicConnectionFactory extends BaseConnectionFactory
 {
-    private AzureDatabase $azureDatabase;
-
-    public function __construct(AzureDatabase $azureDatabase)
+    public function __construct(private ContainerInterface $container, private AzureDatabase $azureDatabase, private LoggerInterface $logger)
     {
-        $this->azureDatabase = $azureDatabase;
-        parent::__construct([]);
+        parent::__construct($container->getParameter('doctrine.dbal.connection_factory.types'));
     }
 
     public function createConnection(array $params, Configuration $config = null, EventManager $eventManager = null, array $mappingTypes = []): DynamicConnection
     {
         $defaultConnection = parent::createConnection($params, $config, $eventManager, $mappingTypes);
+
         $driver = $defaultConnection->getDriver();
 
-        return new DynamicConnection($defaultConnection->getParams(), $driver, $defaultConnection->getConfiguration(), $defaultConnection->getEventManager(), $this->azureDatabase);
+        return new DynamicConnection($defaultConnection->getParams(), $driver, $defaultConnection->getConfiguration(), $defaultConnection->getEventManager(), $this->azureDatabase, $this->logger);
     }
 }
