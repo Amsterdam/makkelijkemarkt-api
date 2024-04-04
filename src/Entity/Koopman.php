31c92b13c9fee8dbb15d5b581aa42c7d485289de
Harm <h.manders@amsterdam.nl>
2024-04-04 17:24:25 +0200

    grumpy

--- src/Entity/Koopman.php
+++ src/Entity/Koopman.php
@@ -261,9 +261,8 @@ class Koopman
 
     /**
      * @var Collection|Vervanger[]
-     * 
-     * @Groups({"vervanger"})
      *
+     * @Groups({"vervanger"})
      * @Groups({"vervanger"})
      *
      * @ORM\OneToMany(targetEntity="Vervanger", mappedBy="vervanger", fetch="EXTRA_LAZY", orphanRemoval=true)
